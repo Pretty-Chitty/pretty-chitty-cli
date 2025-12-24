a662941cea391bbbe962f3f074275565884a871d
Josh Breckman <jbreckman@gmail.com>
2025-12-24 10:25:05 -0500

    Suppress warnings

--- bin/run.js
+++ bin/run.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env -S npx tsx --experimental-import-meta-resolve
 
+// Suppress punycode deprecation warning from dependencies
+process.removeAllListeners('warning');
+process.on('warning', (warning) => {
+  if (warning.name === 'DeprecationWarning' && warning.message.includes('punycode')) {
+    return;
+  }
+  console.warn(warning);
+});
+
 import { cli } from "../src/index.ts";
 
 await cli();
